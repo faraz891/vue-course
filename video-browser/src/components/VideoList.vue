49ea9758d1e31212c167ad5ac34d3f94653cb44c
Stublink <chrisrey95@gmail.com>
2018-12-30 13:31:14 -0500

    created VideoListItem component to display title of a video, used v-for directive to loop through videos prop we are receiving from the App component and passed down each video as a prop to VideoListItem

--- video-browser/src/components/VideoList.vue
+++ video-browser/src/components/VideoList.vue
@@ -1,17 +1,24 @@
 <template>
     <ul>
-        VideoList
-        {{ videos.length }}
+        <VideoListItem 
+            v-for="video in videos"
+            :video="video"
+            :key="video.etag"
+        />
     </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem'
 export default {
     name: 'VideoList',
+    components: {
+        VideoListItem
+    },
     // props: ['videos'],
     props: {
         videos: Array,
-    }
+    },
 }
 </script>
 
