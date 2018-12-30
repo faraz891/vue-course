97666171cc66d882ff895968c0bb6ff5d51bd85e
Stublink <chrisrey95@gmail.com>
2018-12-30 14:04:37 -0500

    display video thumbnail using a computed function, added styling to support thumbnail in VueListItem component

--- video-browser/src/components/VideoListItem.vue
+++ video-browser/src/components/VideoListItem.vue
@@ -1,16 +1,30 @@
 <template>
-    <li class="list-group-item">
-        {{ video.snippet.title }}
+    <li class="list-group-item media">
+        <img class="mr-3" :src="thumbnailUrl" />
+        <div class="media-body">
+            {{ video.snippet.title }}
+        </div>
     </li>
 </template>
 
 <script>
 export default {
     name: 'VideoListItem',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        thumbnailUrl() {
+            return this.video.snippet.thumbnails.default.url
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    li {
+        display: flex;
+        cursor: pointer;
+    }
+    li:hover {
+        background: #eee
+    }
 </style>
