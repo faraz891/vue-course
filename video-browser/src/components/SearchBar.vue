f35a023b7bd8943e2e8430cc6daa882f8282fb43
Stublink <chrisrey95@gmail.com>
2018-12-29 23:57:56 -0500

    Created SearchBar component with input tag where we use the v-on directive shortcut @

---
+++ video-browser/src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- <input v-on:input="onInput"/> -->
+        <input @input="onInput"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onInput: e => {
+            console.log(e.target.value)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
