648afd6fe74fb23ae7d5495b1af24cca3a32f05a
Chris Cedeno <chrisrey95@gmail.com>
2019-01-02 14:28:36 -0500

    created Header component + imported semantic ui style sheet

---
+++ images-project/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a href="/" class="active item">
+            ImageStorage
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
